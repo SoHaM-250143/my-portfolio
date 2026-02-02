5453eccdb706dc4d48a90eb6e87f05e6f9c0a83e
SohaM <mhatresoham2501@gmail.com>
2026-02-02 19:37:16 +0530

    Add UnderUpdation component for maintenance message

---
+++ src/components/UnderUpdation.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function UnderUpdation() {
+  return (
+    <div className="updation-overlay">
+      <div className="updation-box">
+        <h1>🚧 Website Under Updation</h1>
+        <p>
+          I am currently improving this website.<br />
+          Please check back soon.
+        </p>
+      </div>
+    </div>
+  );
+}

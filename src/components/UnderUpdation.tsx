265a9855f34df0d581996df172723575508fcc7d
SohaM <mhatresoham2501@gmail.com>
2026-02-02 19:58:09 +0530

    Refactor message layout in UnderUpdation component
    Split the message into two separate paragraphs.

--- src/components/UnderUpdation.tsx
+++ src/components/UnderUpdation.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import Header from "./Header";
+
 export default function UnderUpdation() {
   return (
     <div className="updation-overlay">
-      <div className="updation-box">
+      {/* Show only logo + name */}
+      <div className="updation-header">
+        <Header />
+      </div>
+
+      {/* Center message */}
+      <div className="updation-center">
         <h1>🚧 Website Under Updation</h1>
-        <p>
-          I am currently improving this website.<br />
-          Please check back soon.
-        </p>
+        <p>I am currently improving this website.</p>
+        <p>Please check back soon.</p>
       </div>
     </div>
   );
